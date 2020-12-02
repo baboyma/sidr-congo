bf2db8e65a83e22b5022966fa3be805a3920f5ed
Baboyma Kagniniwa <baboyma@gmail.com>
2020-12-02 11:41:27 -0500

    Data Summary - adding partners table

--- Scripts/FY20_Q4_DataRequest_HistoryTables.R
+++ Scripts/FY20_Q4_DataRequest_HistoryTables.R
@@ -3,12 +3,16 @@
 ##  PURPOSE: code to create table of results a la jacquie
 ##  LICENCE: MIT
 ##  DATE:    2020-6-16
-##  UPDATE:  2020-11-20
+##  UPDATED:  2020-12-01
 
 # PACKAGES -------------------------------------------------
 
 library(tidyverse)
 library(ICPIutilities)
+
+# SOURCE ---------------------------------------------------
+
+source("../_setup/00_Setup.R")
 
 # GLOBAL --------------------------------------------------
 
@@ -18,28 +22,58 @@ country <- "Democratic Republic of the Congo"
 
 agencies <- c("USAID", "HHS/CDC")
 
+agency <- agencies %>% first()
+
 curr_pd <- 4
 
-curr_fy <- 2020 #Wavelength::curr_fy
+curr_fy <- 2020
 
 fiscal_years <- c(curr_fy - 1, curr_fy)
-#fiscal_years <- c(2019, 2020)
+
+
+# Latest MSD Zipped File
+file_msd <- list.files(
+    path = dir_merdata,
+    pattern = "MER_S.*_PSNU_IM_.*_20201113_v1_1_D.*.zip",
+    full.names = TRUE
+  ) %>%
+  sort() %>%
+  last()
 
 # Indicators ----------------------------------------------------
 
+# Indicators of interest
 ind_list1 <- c("TX_CURR",
               "TX_NET_NEW",
               "TX_NEW",
               "HTS_TST",
               "HTS_TST_POS",
               "TX_RTT",
-              "TX_ML")
+              "TX_ML",
+              "OVC_SERV",
+              "PrEP_CURR",
+              "PrEP_NEW")
 
-ind_list2 <- c("TX_CURR", "TX_NET_NEW", "retention",
-              "TX_NEW", "linkage", "HTS_TST_POS", "positivity",
-              "HTS_TST", "TX_PVLS_D", "coverage", "suppression",
-              "TX_ML", "TX_RTT")
+# Used to arrange data
+ind_list2 <- c("TX_CURR",
+               "TX_NET_NEW",
+               "retention",
+               "TX_NEW",
+               "linkage",
+               "HTS_TST_POS",
+               "positivity",
+               "HTS_TST",
+               "TX_PVLS_D",
+               "coverage",
+               "suppression",
+               "TX_ML",
+               "TX_RTT",
+               "OVC_SERV",
+               "PrEP_CURR",
+               "PrEP_NEW")
 
+
+# Periods
 pd_list <- c("fy2019q1",
              "fy2019q2",
              "fy2019q3",
@@ -53,19 +87,26 @@ pd_list <- c("fy2019q1",
              "fy2020cumulative",
              "fy2020_targets")
 
+# TODO:
+qtrs <- 1:4
+refs <- c("cumulative", "_targets")
+
+pds <- qtrs %>%
+  paste0("q", .) %>%
+  append(refs)
+
+pds_list <- cross3("fy", fiscal_years, pds) %>%
+  map_chr(.x, .f = ~ paste0(.x, collapse = "")) %>%
+  sort()
+
+pds_list
+
 # DATA ------------------------------------------------------
 
-# df <- MSD
-df <- list.files(
-    path = dir_merdata,
-    pattern = "MER_S.*_PSNU_IM_.*_20201113_v1_1.zip",
-    full.names = TRUE
-  ) %>%
-  sort() %>%
-  last() %>%
-  read_msd()
+# MSD
+df <- file_msd %>% read_msd()
 
-df %>% glimpse()
+#df %>% glimpse()
 
 #step 1: everything other than vl/retention
 
@@ -74,40 +115,42 @@ df1 <- df %>%
          indicator %in% ind_list1,
          fiscal_year %in% fiscal_years,
          fundingagency != "Dedup") %>%
-  dplyr::select(fundingagency, snu1, psnu, psnuuid,
+  dplyr::select(fundingagency, primepartner, snu1, psnu, psnuuid,
                 standardizeddisaggregate, otherdisaggregate, sex,
                 trendsfine, indicator, fiscal_year, targets:cumulative) %>%
-  group_by(fundingagency, snu1, psnu, psnuuid, standardizeddisaggregate,
-           otherdisaggregate, sex, trendsfine, fiscal_year, indicator) %>%
+  group_by(fundingagency, primepartner, snu1, psnu, psnuuid,
+           standardizeddisaggregate, otherdisaggregate, sex,
+           trendsfine, fiscal_year, indicator) %>%
   summarise_if(is.numeric, ~sum(., na.rm = TRUE)) %>%
   ungroup() %>%
-  reshape_msd("long")
+  reshape_msd(direction = "long")
 
 #step 2: retention/vl
 
 df_vl <- df %>%
-  filter(indicator %in% c("TX_CURR", "TX_PVLS"),
+  filter(operatingunit == country,
+         indicator %in% c("TX_CURR", "TX_PVLS"),
          fiscal_year %in% fiscal_years,
-         fundingagency != "Dedup",
-         operatingunit == country) %>%
-  mutate(indicator = ifelse(numeratordenom == "D",
-                            paste0(indicator, "_D"),
-                            indicator),
-         indicator = ifelse(numeratordenom == "N",
-                            paste0(indicator, "_N"),
-                            indicator),
+         fundingagency != "Dedup") %>%
+  mutate(indicator = case_when(
+           numeratordenom %in% c("N", "D") ~ paste0(indicator, "_", numeratordenom),
+           TRUE ~ indicator
+         ),
          indicator = case_when(
            indicator == "TX_CURR_N" ~ "TX_CURR",
-           TRUE ~ indicator)) %>%
-  dplyr::select(fundingagency, snu1, psnu, psnuuid, standardizeddisaggregate,
-                sex, trendsfine, indicator, fiscal_year, starts_with("qtr")) %>%
-  group_by(fundingagency, snu1, psnu, psnuuid, standardizeddisaggregate, sex,
+           TRUE ~ indicator
+         )) %>%
+  dplyr::select(fundingagency, primepartner, snu1, psnu, psnuuid,
+                standardizeddisaggregate, sex,
+                trendsfine, indicator, fiscal_year, starts_with("qtr")) %>%
+  group_by(fundingagency, primepartner, snu1, psnu, psnuuid,
+           standardizeddisaggregate, sex,
            trendsfine, fiscal_year, indicator) %>%
   summarise_at(vars(starts_with("qtr")), sum, na.rm = TRUE) %>%
   ungroup() %>%
-  reshape_msd("long") %>%
+  reshape_msd(direction = "long") %>%
   spread(indicator, val) %>%
-  group_by(fundingagency, snu1, psnu, psnuuid,
+  group_by(fundingagency, primepartner, snu1, psnu, psnuuid,
            standardizeddisaggregate, sex, trendsfine) %>%
   mutate(TX_CURR_lag2 = lag(TX_CURR, 2),
          TX_CURR_lag1 = lag(TX_CURR, 1)) %>%
@@ -118,11 +161,19 @@ df_vl <- df %>%
 
 ##  step 3: append
 
-df_cntry <- bind_rows(df1, df_vl)
+df_cntry <- bind_rows(df1, df_vl) %>%
+  mutate(val = as.integer(val))
 
-##  Step 4: create summary table, create vl suppression, coverage, retention
-## summary table
+df_cntry %>%
+  distinct(indicator) %>%
+  pull()
 
+View(df_cntry)
+
+##  Step 4: create summary table,
+##  create vl suppression, coverage, retention
+
+## Country summary table
 df_cntry_all <- df_cntry %>%
   filter(
     standardizeddisaggregate %in% c("Total Numerator", "Total Denominator"),
@@ -142,7 +193,7 @@ df_cntry_all <- df_cntry %>%
   mutate(indicator = factor(indicator, ind_list2),
          period = factor(period, pd_list),
          fundingagency = str_remove(fundingagency, "HHS/"),
-         fundingagency = factor(fundingagency, c("USAID", "CDC"))) %>%
+         fundingagency = factor(fundingagency, levels = c("USAID", "CDC"))) %>%
   spread(period, val)
 
 # Export Country summary tbl
@@ -152,7 +203,7 @@ write_csv(x = df_cntry_all,
                                   " - Overview_tbl_q",
                                   curr_pd,
                                   "_", format(Sys.Date(), "%Y%m%d"),
-                                  ".csv")))
+                                  ".csv")), na = "")
 
 # SNU1 Summary
 df_snu1_all <- df_cntry %>%
@@ -184,8 +235,7 @@ write_csv(x = df_snu1_all,
                                   " - Overview_snu1_tbl_q",
                                   curr_pd,
                                   "_", format(Sys.Date(), "%Y%m%d"),
-                                  ".csv")),
-          na = "")
+                                  ".csv")), na = "")
 
 # PSNU Summary
 df_psnu_all <- df_cntry %>%
@@ -217,10 +267,40 @@ write_csv(x = df_psnu_all,
                                   " - Overview_psnu_tbl_q",
                                   curr_pd,
                                   "_", format(Sys.Date(), "%Y%m%d"),
-                                  ".csv")),
-          na = "")
+                                  ".csv")), na = "")
 
 
+# Partners Summary
+df_partner_all <- df_cntry %>%
+  filter(
+    standardizeddisaggregate %in% c("Total Numerator", "Total Denominator"),
+    fundingagency %in% agencies
+  ) %>%
+  group_by(fundingagency, primepartner, indicator, period) %>%
+  summarise_if(is.numeric, ~ sum(., na.rm = TRUE)) %>%
+  spread(indicator, val) %>%
+  mutate(suppression = round(TX_PVLS_D / TX_CURR_lag2 * 100, 2),
+         coverage = round(TX_PVLS_N / TX_PVLS_D * 100, 2),
+         retention = round(TX_CURR / (TX_CURR_lag1 + TX_NEW) * 100, 2),
+         linkage = round(TX_NEW / HTS_TST_POS * 100, 2),
+         positivity = round(HTS_TST_POS / HTS_TST * 100, 2)) %>%
+  ungroup %>%
+  dplyr::select(-TX_CURR_lag1, -TX_CURR_lag2, -TX_PVLS_N) %>%
+  gather(indicator, val, HTS_TST:positivity) %>%
+  mutate(indicator = factor(indicator, ind_list2),
+         period = factor(period, pd_list),
+         fundingagency = str_remove(fundingagency, "HHS/"),
+         fundingagency = factor(fundingagency, c("USAID", "CDC"))) %>%
+  spread(period, val)
+
+# Export psnu summary tbl
+write_csv(x = df_partner_all,
+          file = file.path(dir_dataout,
+                           paste0(country,
+                                  " - Overview_partners_tbl_q",
+                                  curr_pd,
+                                  "_", format(Sys.Date(), "%Y%m%d"),
+                                  ".csv")), na = "")
 
 ## table for peds
 df_cntry_peds <- df_cntry %>%
@@ -250,8 +330,7 @@ write_csv(x = df_cntry_peds,
                                   " - Overview_peds_tbl_q",
                                   curr_pd,
                                   "_", format(Sys.Date(), "%Y%m%d"),
-                                  ".csv")),
-          na = "")
+                                  ".csv")), na = "")
 
 
 ## table for peds x snu1
@@ -282,8 +361,71 @@ write_csv(x = df_cntry_peds_by_snu1,
                                   " - Overview_peds_x_snu1_tbl_q",
                                   curr_pd,
                                   "_", format(Sys.Date(), "%Y%m%d"),
-                                  ".csv")),
-          na = "")
+                                  ".csv")), na = "")
+
+
+## table for peds x psnu
+df_cntry_peds_by_psnu <- df_cntry %>%
+  filter(trendsfine %in% c("<01", "01-09", "10-14"),
+         fundingagency %in% agencies) %>%
+  group_by(fundingagency, snu1, psnu, psnuuid, indicator, period) %>%
+  summarise_if(is.numeric, ~ sum(., na.rm = TRUE)) %>%
+  spread(indicator, val) %>%
+  mutate(suppression = round(TX_PVLS_D / TX_CURR_lag2 * 100, 2),
+         coverage = round(TX_PVLS_N / TX_PVLS_D * 100, 2),
+         retention = round(TX_CURR / (TX_CURR_lag1 + TX_NEW) * 100, 2),
+         linkage = round(TX_NEW / HTS_TST_POS * 100, 2),
+         positivity = round(HTS_TST_POS / HTS_TST * 100, 2)) %>%
+  ungroup %>%
+  dplyr::select(-TX_CURR_lag1, -TX_CURR_lag2, -TX_PVLS_N) %>%
+  gather(indicator, val, HTS_TST:positivity) %>%
+  mutate(indicator = factor(indicator, ind_list2),
+         period = factor(period, pd_list),
+         fundingagency = str_remove(fundingagency, "HHS/"),
+         fundingagency = factor(fundingagency, c("USAID", "CDC"))) %>%
+  spread(period, val)
+
+# Export
+write_csv(x = df_cntry_peds_by_psnu,
+          file = file.path(dir_dataout,
+                           paste0(country,
+                                  " - Overview_peds_x_psnu_tbl_q",
+                                  curr_pd,
+                                  "_", format(Sys.Date(), "%Y%m%d"),
+                                  ".csv")), na = "")
+
+
+## table for peds x partners
+df_cntry_peds_by_partners <- df_cntry %>%
+  filter(trendsfine %in% c("<01", "01-09", "10-14"),
+         fundingagency %in% agencies) %>%
+  group_by(fundingagency, primepartner, indicator, period) %>%
+  summarise_if(is.numeric, ~ sum(., na.rm = TRUE)) %>%
+  spread(indicator, val) %>%
+  mutate(suppression = round(TX_PVLS_D / TX_CURR_lag2 * 100, 2),
+         coverage = round(TX_PVLS_N / TX_PVLS_D * 100, 2),
+         retention = round(TX_CURR / (TX_CURR_lag1 + TX_NEW) * 100, 2),
+         linkage = round(TX_NEW / HTS_TST_POS * 100, 2),
+         positivity = round(HTS_TST_POS / HTS_TST * 100, 2)) %>%
+  ungroup %>%
+  dplyr::select(-TX_CURR_lag1, -TX_CURR_lag2, -TX_PVLS_N) %>%
+  gather(indicator, val, HTS_TST:positivity) %>%
+  mutate(indicator = factor(indicator, ind_list2),
+         period = factor(period, pd_list),
+         fundingagency = str_remove(fundingagency, "HHS/"),
+         fundingagency = factor(fundingagency, c("USAID", "CDC"))) %>%
+  spread(period, val)
+
+# Export
+write_csv(x = df_cntry_peds_by_partners,
+          file = file.path(dir_dataout,
+                           paste0(country,
+                                  " - Overview_peds_x_partners_tbl_q",
+                                  curr_pd,
+                                  "_", format(Sys.Date(), "%Y%m%d"),
+                                  ".csv")), na = "")
+
+
 
 
 
